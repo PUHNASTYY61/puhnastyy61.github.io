bda0e0a0c3644dce492f4b4071254a723b6d942b
PUHNASTYY61 <asitkovkirill@gmail.com>
2025-06-27 23:03:15 +0300

    Update shake-animation.js

--- shake-animation.js
+++ shake-animation.js
@@ -1,7 +1,8 @@
-const heading = document.gerElementById('shake-heading');
+const heading = document.getElementById('shake-heading');
 const amplitude = 15;
 const speed = 0.01;
 let time = 0;
+
 function animateShake() {
   time += 1;
   const offset = Math.sin(time * speed) * amplitude;
